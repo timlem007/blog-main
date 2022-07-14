27de280a451b588b428beb1b6ad761295ed77196
timlem007 <abd_temur@mail.ru>
2022-07-14 22:48:44 +0300

    ev

--- src/redux/actionCreators/actionCreators.js
+++ src/redux/actionCreators/actionCreators.js
@@ -24,7 +24,7 @@ export const getArticles =
   async (dispatch) => {
     dispatch({ type: LOADING_ARTICLES });
     try {
-      const result = await fetch(`https://kata.academy:8021/api/articles?limit=5&offset=${(page - 1) * 5}`, {
+      const result = await fetch(`https://kata.academy:8022/api/articles?limit=5&offset=${(page - 1) * 5}`, {
         headers: { 'Content-Type': 'application/json;charset=utf-8', Authorization: token ? `Token ${token}` : null },
       });
       const json = await result.json();
@@ -41,7 +41,7 @@ export const getFullArticle =
   async (dispatch) => {
     dispatch({ type: LOADING_ARTICLE });
     try {
-      const result = await fetch(`https://kata.academy:8021/api/articles/${slug}`, {
+      const result = await fetch(`https://kata.academy:8022/api/articles/${slug}`, {
         headers: { 'Content-Type': 'application/json;charset=utf-8', Authorization: token ? `Token ${token}` : null },
       });
 
@@ -58,7 +58,7 @@ export const getFullArticle =
 export const register = (data) => async (dispatch) => {
   try {
     dispatch({ type: ACCOUNT_LOADING });
-    const result = await fetch(`https://kata.academy:8021/api/users`, {
+    const result = await fetch(`https://kata.academy:8022/api/users`, {
       method: 'POST',
       body: JSON.stringify({
         user: {
@@ -88,7 +88,7 @@ export const register = (data) => async (dispatch) => {
 export const authorization = (email, password) => async (dispatch) => {
   try {
     dispatch({ type: ACCOUNT_LOADING });
-    const result = await fetch(`https://kata.academy:8021/api/users/login`, {
+    const result = await fetch(`https://kata.academy:8022/api/users/login`, {
       method: 'POST',
       body: JSON.stringify({
         user: {
@@ -119,7 +119,7 @@ export const logOut = () => {
 export const like = (slug, liked) => async (dispatch) => {
   const token = JSON.parse(localStorage.getItem('user')).token;
   try {
-    const result = await fetch(`https://kata.academy:8021/api/articles/${slug}/favorite`, {
+    const result = await fetch(`https://kata.academy:8022/api/articles/${slug}/favorite`, {
       method: !liked ? 'POST' : 'DELETE',
       headers: { 'Content-Type': 'application/json;charset=utf-8', Authorization: `Token ${token}` },
     });
@@ -135,7 +135,7 @@ export const like = (slug, liked) => async (dispatch) => {
 export const deletePost = (slug, token) => async (dispatch) => {
   try {
     dispatch({ type: LOADING_ARTICLE });
-    const result = await fetch(`https://kata.academy:8021/api/articles/${slug}`, {
+    const result = await fetch(`https://kata.academy:8022/api/articles/${slug}`, {
       method: 'DELETE',
       headers: { 'Content-Type': 'application/json;charset=utf-8', Authorization: token ? `Token ${token}` : null },
     });
@@ -156,7 +156,7 @@ export const editProfile = (username, email, password, image, token) => async (d
       error.code = 422;
       throw error;
     }
-    const result = await fetch(`https://kata.academy:8021/api/user`, {
+    const result = await fetch(`https://kata.academy:8022/api/user`, {
       method: 'PUT',
       body: JSON.stringify({
         user: {
@@ -190,7 +190,7 @@ export const editProfile = (username, email, password, image, token) => async (d
 export const createArticle = (title, description, body, tagList, token, edit, slug) => async (dispatch) => {
   try {
     dispatch({ type: ACCOUNT_LOADING });
-    const result = await fetch(`https://kata.academy:8021/api/articles/${edit ? slug : ''}`, {
+    const result = await fetch(`https://kata.academy:8022/api/articles/${edit ? slug : ''}`, {
       method: edit ? 'PUT' : 'POST',
       body: JSON.stringify({
         article: {
